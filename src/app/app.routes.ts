ff63fe0cdaca864e3e15e6d8602d769189af0cdb
Brian Widhalm <bdwidhalm@gmail.com>
2025-09-22 16:42:24 -0600

    adding components and service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Songs } from './songs/songs';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+        title: 'Music Manager'
+    },
+    {
+        path: 'songs',
+        component: Songs,
+        title: 'Our Songs'
+    }
+];
---
+++ src/app/songs/songs.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+
+@Component({
+  selector: 'app-songs',
+  imports: [MatTableModule],
+  templateUrl: './songs.html',
+  styleUrl: './songs.css'
+})
+export class Songs {
+  displayedColumns: string[] = ['artist', 'title', 'id'];
+  dataSource = [{
+    "id": "64963fe360c217d37477bf55",
+    "file": "/Users/brianwidhalm/Music/Chris Tomlin/Whom Shall I Fear (God of Angel Armies).mp3",
+    "relativePath": null,
+    "album": null,
+    "album_artist": null,
+    "artist": "Chris Tomlin",
+    "best_release_date": null,
+    "genre": null,
+    "non_std_genre": null,
+    "original_release_date": null,
+    "publisher": null,
+    "recording_date": null,
+    "release_date": null,
+    "title": "Whom Shall I Fear (God of Angel Armies)",
+    "track_num": "CountAndTotalTuple(count=None, total=None)"
+  },
+  {
+    "id": "64963fe360c217d37477bf56",
+    "file": "/Users/brianwidhalm/Music/Chris Tomlin/Precious Love.mp3",
+    "relativePath": null,
+    "album": null,
+    "album_artist": null,
+    "artist": "Chris Tomlin",
+    "best_release_date": null,
+    "genre": null,
+    "non_std_genre": null,
+    "original_release_date": null,
+    "publisher": null,
+    "recording_date": null,
+    "release_date": null,
+    "title": "Precious Love",
+    "track_num": "CountAndTotalTuple(count=None, total=None)"
+  },
+  {
+    "id": "64963fe360c217d37477bf57",
+    "file": "/Users/brianwidhalm/Music/Chris Tomlin/Our God.mp3",
+    "relativePath": null,
+    "album": null,
+    "album_artist": null,
+    "artist": "Chris Tomlin",
+    "best_release_date": null,
+    "genre": null,
+    "non_std_genre": null,
+    "original_release_date": null,
+    "publisher": null,
+    "recording_date": null,
+    "release_date": null,
+    "title": "Our God",
+    "track_num": "CountAndTotalTuple(count=None, total=None)"
+  }]
+}
